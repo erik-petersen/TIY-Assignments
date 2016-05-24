6329b900cc12d13ab153142e0225f9b3df48f174
Erik Petersen <contacterikpetersen@gmail.com>
2016-05-23 20:28:25 -0400

    Initial commit

---
+++ three-and-five.js
@@ -0,0 +1,13 @@
+var sum = 0;
+
+for( var i = 0; i < 1000; i++ ) {
+  if( i % 3 === 0 ) {
+    sum += i;
+  } else if ( i % 5 === 0 ) {
+    sum += i;
+  } else {
+    sum += 0;
+  }
+}
+
+console.log( sum );
