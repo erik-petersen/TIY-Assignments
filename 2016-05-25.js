dada80da7e2ff97b8ec9ee590ed1338e2c456439
Erik Petersen <contacterikpetersen@gmail.com>
2016-05-25 10:38:23 -0400

    Initial commit

---
+++ 2016-05-25.js
@@ -0,0 +1,8 @@
+var currYear = 2016;
+var birthYear = 1982;
+console.log( "They are either " + (currYear-birthYear-1) + " or " + (currYear-birthYear) );
+
+// or
+
+var ages = { name: "Jen", birthYear: 1982};
+console.log( "They are either " + (currYear-ages.birthYear-1) + " or " + (currYear-ages.birthYear) );
