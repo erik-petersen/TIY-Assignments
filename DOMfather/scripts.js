418191e22420c91a3f9ce8eab87d4d0d33d9c54e
Erik Petersen <contacterikpetersen@gmail.com>
2016-05-25 19:45:44 -0400

    Cleaned up code and commented some

--- DOMfather/scripts.js
+++ DOMfather/scripts.js
@@ -1,32 +1,39 @@
-// Using JavaScript, change the body's tag style so it has a font-family of the sans-serif font stack of your choice.
+// Erik Petersen Homework 2016-05-25: "About Me"
+
+// Change the body's tag style so it has a font-family of the sans-serif font stack
 document.body.style.fontFamily = "helvetica, verdana, sans-serif";
 
-//Using JavaScript, replace each of the spans with your own information.
+//Replace each of the spans with my own information.
 var myName = "Erik Petersen";
 var myTown = "Cary, NC";
 var myMovie = "Tommy Boy";
 var myFood = "Spaghetti";
 
+//Grab the elements with these IDs and insert values between the tags
 document.getElementById( "fullname" ).innerHTML = myName;
 document.getElementById( "hometown" ).innerHTML = myTown;
 document.getElementById( "movie" ).innerHTML = myMovie;
 document.getElementById( "food" ).innerHTML = myFood;
 
-//Iterate through each li and change the class to listitem. Add a style tag that sets a rule
-//for listitem to make the color red.
+//Create an array to store the list of li elements
 var listItems = document.getElementsByTagName( "li" );
 
+//Create function to change the class name of elements
 function changeClass( elements ) {
   for( var index = 0; index < elements.length; index++ ) {
     elements[index].setAttribute( "class", "listitem" );
-    // elements[index].style.color = "red";
   }
 }
 
+//Call changeClass
 changeClass( listItems );
-document.getElementsByClassName( "listitem" ).style.color = "red";
 
-//Create a new img element and set its src attribute to a picture of you. Append that element to the page.
+//Add a style tag that sets a rule for listitem to make the color red
+for( var index = 0; index < listItems.length; index++ ) {
+  listItems[index].style.color = "red";
+}
+
+//Create a new img element and set its src attribute to a picture of me and then append it to the page
 var selfPic = document.createElement( "img" );
 selfPic.setAttribute( "src", "assets/images/erik-headshot.jpg" );
 selfPic.setAttribute( "alt", "Picture of Me" );
