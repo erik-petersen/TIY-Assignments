3670ee0d5821c49f3d5cc9c3c14a262b83880d6a
Erik Petersen <contacterikpetersen@gmail.com>
2016-05-26 10:30:10 -0400

    Initial commit

---
+++ 2016-05-26.js
@@ -0,0 +1,8 @@
+// Daily code challenge 2016-05-26
+
+var myCurAge = 40;
+var myMaxAge = 120;
+var snackDailyCost = 5;
+var snacksPerYear = 365;
+var totalCost = (myMaxAge - myCurAge) * snacksPerYear * snackDailyCost;
+console.log( "I will need $" + totalCost + " to buy me enough Goodberry's to last me until the ripe old age of " + myMaxAge );
