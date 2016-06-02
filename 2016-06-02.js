06a26e0080b498fd369f5331c46b659cde88c878
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-02 11:17:36 -0400

    Initial commit

---
+++ 2016-06-02.js
@@ -0,0 +1,11 @@
+<!-- Code challenge 2016-06-02 -->
+
+for( var i = 0; i <= 20; i++ ) {
+	if( i % 2 !== 0 ) {
+		console.log( i + " is odd" );
+	}
+
+	else {
+		console.log( i + " is even" );
+	}
+}
