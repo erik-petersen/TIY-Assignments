9c12b48874b4878aeb1a1b9f46a3b77ab4a9407f
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-06 10:29:28 -0400

    Switched from plus to concat

--- 2016-06-06.js
+++ 2016-06-06.js
@@ -2,14 +2,14 @@ var topDesserts = [ "Apple Crostada", "Goodberry's", "Chocolate Pie" ];
 
 for( var index = 0; index < topDesserts.length; index++ ) {
 	if( index === 0 ) {
-		console.log( "My 1st dessert choice is " + topDesserts[index] );
+		console.log( "My 1st dessert choice is ".concat( topDesserts[index]) );
 	}
 
 	else if( index === 1 ) {
-		console.log( "My 2nd dessert choice is " + topDesserts[index] );
+		console.log( "My 2nd dessert choice is ".concat( topDesserts[index]) );
 	}
 
 	else {
-		console.log( "My 3rd dessert choice is " + topDesserts[index] );
+		console.log( "My 3rd dessert choice is ".concat( topDesserts[index]) );
 	}
 }
