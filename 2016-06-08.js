aa66c346e26e9640dad084ae76a888f8c877d5bd
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-08 10:18:48 -0400

    Initial re-commit

--- 2016-06-08.js
+++ 2016-06-08.js
@@ -1,4 +1,7 @@
-var favMovie = { title: "Tommy Boy", duration: 90, stars: ["Chris Farley", " David Spade", " Dan Aykroyd", " Julie Warner", " Bo Derek", " and others"] };
+var favMovie = {
+                title: "Tommy Boy",
+                duration: 90,
+                stars: ["Chris Farley", " David Spade", " Dan Aykroyd", " Julie Warner", " Bo Derek", " and others"] };
 
 function displayMovie ( movie ) {
 	console.log(movie.title + " lasts for " + movie.duration + " minutes." + "\n\nStars: " + movie.stars);
