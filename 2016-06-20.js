77b26b190c1a64c0abd248548d34174f6915b4c8
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-20 10:41:11 -0400

    Initial commit

---
+++ 2016-06-20.js
@@ -0,0 +1,15 @@
+function notBad( str ) {
+	var notIndex = str.indexOf( "not" );
+	var badIndex = str.indexOf( "bad" );
+
+	if( str.includes( "not") && notIndex < badIndex ) {
+		return str.substring( 0, notIndex ) + "good";
+	}
+
+	else {
+		return str;
+	}
+}
+
+console.log( notBad("This dinner is not that bad!") );
+console.log( notBad( "This dinner is bad!") );
