77adf1fca85045ef91ebd2abacee31bfdca88775
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-30 09:51:27 -0400

    Initial commit

--- 2016-06-30.js
+++ 2016-06-30.js
@@ -1,5 +1,6 @@
 function frequent( str1, str2 ) {
 	var numTimes = 0;
+	var plurality;
 
 	for( var index = 0; index < str2.length; index++ ) {
 		if( str2.charAt(index) == str1 ) {
@@ -7,7 +8,15 @@ function frequent( str1, str2 ) {
 		}
 	}
 
-	return str2 + " contains the letter " + '"' + str1 + '"' + " " + numTimes + " times";
+	if( numTimes < 2 ) {
+		plurality = "time";
+	}
+
+	else {
+		plurality = "times";
+	}
+
+	return str2 + " contains the letter " + '"' + str1 + '"' + " " + numTimes + " " + plurality;
 }
 
 console.log( frequent( "o", "victoria" ) );
